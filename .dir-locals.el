498769d2cf92f4c6835111015614174f96c1ae48
iz <iosifzota@protonmail.com>
2018-05-02 23:09:11 +0300

    Project files.

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (cmake-ide-build-dir . "build"))
+ (c++-mode
+  (flycheck-clang-language-standard . "c++11")
+  (flycheck-gcc-language-standard . "c++11")))
+
+
