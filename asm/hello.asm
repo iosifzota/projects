7475f10cacef1eb2f03075fa949bd577f0ec2aea
iz <iosifzota@protonmail.com>
2018-05-15 04:38:35 +0300

    Comments.

--- asm/hello.asm
+++ asm/hello.asm
@@ -5,13 +5,13 @@ section .text
     global _start
 
 _start:
-    mov     rax, 1
-    mov     rdi, 1
-    mov     rsi, msg
-    mov     rdx, 13
+    mov     rax, 1      ; Syscall: sys_write.
+    mov     rdi, 1      ; 1st arg: file descriptor = stdout.
+    mov     rsi, msg    ; 2nd arg: const char* buf.
+    mov     rdx, 13     ; 3rd arg: buf count.
     syscall
 
-    mov     rax, 60
-    mov     rdi, 0
+    mov     rax, 60     ; Syscall (sys_exit)
+    mov     rdi, 0      ; Exit code.
     syscall
 
