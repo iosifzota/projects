31e99b0a719be843e66b9dbf68b5fb3895137430
iz <iosifzota@protonmail.com>
2018-05-28 19:32:53 +0300

    hash_table: More tests.

--- test_hashes.cpp
+++ test_hashes.cpp
@@ -15,15 +15,7 @@ unsigned test_djb_hash(const char*);
 unsigned test_std_hash(const char*);
 unsigned test_fnv_hash(const char*);
 
-void test_story();
-void test_set();
-
-int main()
-{
-        test_set();
-        test_story();
-        return 0;
-}
+void test_scenario(const char* scenario_name, const char* input_file);
 
 #if  defined (__GNUC__) || defined(__GNUG__)
 #define STRINGS_SET_FILE "../input/rand_strings.txt"
@@ -33,17 +25,37 @@ int main()
 #define STORY_FILE "Story.txt"
 #endif
 
-void test_set()
+int main(int argc, char *argv[])
 {
-        test_wrap("Set",
+        test_scenario("Set", STRINGS_SET_FILE);
+        test_scenario("Story", STORY_FILE);
+
+        if (argc > 1) {
+            std::cout << "More tests...\n";
+        }
+
+        for (int i = 1; i < argc; ++i) {
+            const char* scenario_name = argv[i];
+
+            i += 1;
+            req(i < argc, "Usage: test_hash [scenario_name file_name]");
+            test_scenario(scenario_name, argv[i]);
+        }
+
+        return 0;
+}
+
+void test_scenario(const char* scenario_name, const char* input_file)
+{
+        test_wrap(scenario_name,
                   std::vector<Hash_Stats> data;
 
-                  data.push_back({ "Add_Hash", test_add_hash(STRINGS_SET_FILE) });
-                  data.push_back({ "Xor_Hash", test_xor_hash(STRINGS_SET_FILE) });
-                  data.push_back({ "Rot_Hash", test_rot_hash(STRINGS_SET_FILE) });
-                  data.push_back({ "Djb_Hash", test_djb_hash(STRINGS_SET_FILE) });
-                  data.push_back({ "Std_Hash", test_std_hash(STRINGS_SET_FILE) });
-                  data.push_back({ "FNV_Hash", test_fnv_hash(STRINGS_SET_FILE) });
+                  data.push_back({ "Add_Hash", test_add_hash(input_file) });
+                  data.push_back({ "Xor_Hash", test_xor_hash(input_file) });
+                  data.push_back({ "Rot_Hash", test_rot_hash(input_file) });
+                  data.push_back({ "Djb_Hash", test_djb_hash(input_file) });
+                  data.push_back({ "Std_Hash", test_std_hash(input_file) });
+                  data.push_back({ "FNV_Hash", test_fnv_hash(input_file) });
 
                   std::cout << "\nStats:\n";
                   std::sort(data.begin(), data.end());
@@ -53,25 +65,6 @@ void test_set()
                 );
 }
 
-void test_story()
-{
-        test_wrap("Story",
-                  std::vector<Hash_Stats> data;
-
-                  data.push_back({ "Add_Hash", test_add_hash(STORY_FILE) });
-                  data.push_back({ "Xor_Hash", test_xor_hash(STORY_FILE) });
-                  data.push_back({ "Rot_Hash", test_rot_hash(STORY_FILE) });
-                  data.push_back({ "Djb_Hash", test_djb_hash(STORY_FILE) });
-                  data.push_back({ "Std_Hash", test_std_hash(STORY_FILE) });
-                  data.push_back({ "FNV_Hash", test_fnv_hash(STORY_FILE) });
-
-                  std::cout << "\nStats:\n";
-                  std::sort(data.begin(), data.end());
-                  for (const auto& hash : data) {
-                          std::cout << hash; nl;
-                  }
-                );
-}
 
 #define generate_test_hash_body(Hash_Name)                      \
         iz::htable< std::string, int, Hash_Name > test(15000U); \
