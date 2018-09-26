a5badec909f2c25b1c467719f8d3877519fdfe3f
iosifzota <iosifzota@protonmail.com>
2018-09-26 04:55:54 +0300

    Deleted .dir-locals

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (cmake-ide-build-dir . "build"))
- (c++-mode
-  (flycheck-clang-language-standard . "c++11")
-  (flycheck-gcc-language-standard . "c++11")))
